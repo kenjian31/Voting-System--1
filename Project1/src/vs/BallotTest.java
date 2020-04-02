0fcf91b82a5e1884555b8b82fa023e551247f970
jian <wang8636@umn.edu>
2020-04-02 00:46:28 -0500

    added

--- Project1/src/vs/BallotTest.java
+++ Project1/src/vs/BallotTest.java
@@ -19,6 +19,7 @@ class BallotTest {
 		String result2 = test2.toString();
 		String result3 = test3.toString();
 		
+	
 		assertNotSame(Exresult1, result1);
 		assertNotSame(Exresult2, result2);
 		assertNotSame(Exresult3, result3);
--- Project1/src/vs/DroopQuotaTest.java
+++ Project1/src/vs/DroopQuotaTest.java
@@ -23,5 +23,38 @@ class DroopQuotaTest {
 		assertEquals(myTest.ballotCount, 20);
 	
 	}
+	
+	@Test
+	public void testDroopQuotaGenerateAudit() throws IOException {
+		String current = new java.io.File( "." ).getCanonicalPath();
+		String testFileLocation = current+"/testing/plurality_test_5000b_4c.csv";
+		
+		DroopQuota myTest = new DroopQuota(4, testFileLocation);
+		assertEquals(myTest.winner.size(), 0);
+		myTest.ReadFile();
+		myTest.GenerateAudit();
+		assertEquals(myTest.winner.size(), 4);
+		
+		DroopQuota myTest2 = new DroopQuota(0, testFileLocation);
+		assertEquals(myTest2.winner.size(), 0);
+		myTest2.ReadFile();
+		myTest2.GenerateAudit();
+		assertEquals(myTest2.winner.size(), 0);
+		
+
+		//random case 
+		DroopQuota myTest3 = new DroopQuota(1, testFileLocation);
+		assertEquals(myTest3.winner.size(), 0);
+		myTest3.ReadFile();
+		myTest3.GenerateAudit();
+		assertEquals(myTest3.winner.size(), 1);
+
+		DroopQuota myTest4 = new DroopQuota(1000, testFileLocation);
+		assertEquals(myTest4.winner.size(), 0);
+		myTest4.ReadFile();
+		myTest4.GenerateAudit();
+		assertEquals(myTest4.winner.size(), 4);
+       	
+	}
 
 }
