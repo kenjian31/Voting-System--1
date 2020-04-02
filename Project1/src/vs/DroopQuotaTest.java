0bae159a8e78bc10275ec2ecd6cd84812aad5c88
huan1852 <huan1852@umn.edu>
2020-04-01 23:51:16 -0700

    Add some test case

--- Project1/src/vs/DroopQuotaTest.java
+++ Project1/src/vs/DroopQuotaTest.java
@@ -7,7 +7,29 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 class DroopQuotaTest {
-
+	
+	@Test
+    public void testQuotaSeats() throws IOException {
+		
+		// test file 
+		// 20 ballots 
+		// 7 candidates 
+		String current = new java.io.File( "." ).getCanonicalPath();
+		String testFileLocation = current+"/testing/DQ_test_20b_7c.csv";
+		
+		DroopQuota myTest = new DroopQuota(1, testFileLocation);
+		assertEquals(myTest.total_seat, 1);
+		
+		DroopQuota myTest2 = new DroopQuota(4, testFileLocation);
+		assertEquals(myTest2.total_seat, 4);
+		
+		DroopQuota myTest3 = new DroopQuota(100, testFileLocation);
+		assertEquals(myTest3.total_seat, 100);
+		
+		DroopQuota myTest4 = new DroopQuota(10000, testFileLocation);
+		assertEquals(myTest4.total_seat, 10000);
+       	
+	}
 	@Test
 	void DroopQuotaReadFile() throws IOException {
 		///Users/frankchen/Desktop/team5/repo-Team5/Project1/testing/DQ_test_20b_7c.csv
@@ -27,7 +49,7 @@ class DroopQuotaTest {
 	@Test
 	public void testDroopQuotaGenerateAudit() throws IOException {
 		String current = new java.io.File( "." ).getCanonicalPath();
-		String testFileLocation = current+"/testing/plurality_test_5000b_4c.csv";
+		String testFileLocation = current+"/testing/DQ_test_20b_7c.csv";
 		
 		DroopQuota myTest = new DroopQuota(4, testFileLocation);
 		assertEquals(myTest.winner.size(), 0);
@@ -43,12 +65,18 @@ class DroopQuotaTest {
 		
 
 		//random case 
-		DroopQuota myTest3 = new DroopQuota(1, testFileLocation);
+		DroopQuota myTest3 = new DroopQuota(0, testFileLocation);
 		assertEquals(myTest3.winner.size(), 0);
 		myTest3.ReadFile();
 		myTest3.GenerateAudit();
-		assertEquals(myTest3.winner.size(), 1);
-
+		assertEquals(myTest3.winner.size(), 0);
+		
+		DroopQuota myTest5 = new DroopQuota(2, testFileLocation);
+		assertEquals(myTest5.winner.size(), 0);
+		myTest3.ReadFile();
+		myTest3.GenerateAudit();
+		assertEquals(myTest5.winner.size(), 2);
+		
 		DroopQuota myTest4 = new DroopQuota(1000, testFileLocation);
 		assertEquals(myTest4.winner.size(), 0);
 		myTest4.ReadFile();
@@ -58,3 +86,4 @@ class DroopQuotaTest {
 	}
 
 }
+
